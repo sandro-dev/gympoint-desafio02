ad39a7b02324307d91222b4ce9e941e502f59c41
Sandro Santos <sandro.dev@gmail.com>
2019-10-22 23:24:56 -0300

    ESLint and Prettier configuration

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'The node server is running on port: 3333' });
-});
+routes.get('/', (req, res) =>
+  res.json({ message: 'The node server is running on port: 3333' })
+);
 
 export default routes;
