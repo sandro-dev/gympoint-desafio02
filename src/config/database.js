a1a34674d083fe1d080ea0ce0f776ac0babe8ff0
Sandro Santos <sandro.dev@gmail.com>
2019-10-23 00:08:19 -0300

    pg and pg-hstore libraries; sequelize configuration for postgres; migration file

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: '192.168.99.100',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'gympoint',
   define: {
     timestamp: true,
     underscored: true,
