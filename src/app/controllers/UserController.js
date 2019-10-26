3153645b9ee56e8d4762662ad526d046eab1f793
Sandro Santos <sandro.dev@gmail.com>
2019-10-26 17:26:43 -0300

    validations of entry data on method store of UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,32 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
     const user = await User.create(req.body);
 
-    return res.json({ ok: true, User: user });
+    return res.json(user);
   }
 
   async update(req, res) {
