598249b41b18a9e0362de322d7f2dfaddc005dbf
Sandro Santos <sandro.dev@gmail.com>
2019-10-26 16:40:24 -0300

    Session controller with authentication that generate a JWT token

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) =>
   res.json({ message: 'The node server is running on port: 3333' })
 );
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
